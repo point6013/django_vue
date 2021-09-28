bbda689c16fc780abfb683b3319d200db7a1a51d
point6013 <huangmeng891211@126.com>
2021-09-28 13:20:59 +0800

    vue 三个组件

---
+++ drf_vue_blog/frontend/src/components/BlogHeader.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="header">
+        <h1>my django vue blog</h1>
+        <hr>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "BlogHeader.vue"
+    }
+</script>
+
+<style scoped>
+    #header {
+        text-align: center;
+        margin-top: 20px;
+    }
+
+</style>
