36fb7af7aab3418c5861d2371b9ee5b7afa5adc4
point6013 <huangmeng891211@126.com>
2021-09-28 13:21:41 +0800

    vue 三个组件

---
+++ drf_vue_blog/frontend/src/App.vue
@@ -0,0 +1,29 @@
+<!--   frontend/src/App.vue   -->
+
+<template>
+    <BlogHeader/>
+    <ArticleList/>
+    <BlogFooter/>
+</template>
+
+<script>
+    import BlogHeader from "./components/BlogHeader.vue"
+    import BlogFooter from "./components/BlogFooter";
+    import ArticleList from "./components/ArticleList";
+
+    export default {
+        name: 'App',
+        components: {BlogFooter, BlogHeader, ArticleList}
+    }
+</script>
+
+<style>
+
+    #app {
+        font-family: Georgia, Arial, sans-serif;
+        margin-left: 40px;
+        margin-right: 40px;
+    }
+
+
+</style>
